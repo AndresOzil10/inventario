25aea2fb153c315d69a8552ca9ece8a075578ad2
AndresOzil10 <andresref29@gmail.com>
2024-09-19 17:04:13 -0600

    version 4

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:go_router/go_router.dart';
 import 'package:inventario/widgets/custom_info_tarjet.dart';
 
@@ -37,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final jsonResponse = json.decode(response.data);
     setState(() {
       items = jsonResponse[0];
-      print(items);
+      //print(items);
     });
  }
 
@@ -50,66 +51,9 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         title: const Text("Inventario Equipos Kayser"),
       ),
-      body: GridView.count(
-        crossAxisCount: 4,
-        children:[
-        CustomInfo(function: () {}, label: Text('Laptops Asignadas: 80'), icon: Icon(Icons.laptop), style: ButtonStyle(backgroundColor: WidgetStateProperty.all(const Color(0xffe12c58)))),
-        FilledButton.icon(onPressed: () {}, label: const Text('Pc´s Asignadas: 28'), icon: const Icon(Icons.computer)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Garburgs Asignadas: 42'), icon: const Icon(Icons.print_rounded)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Impresoras RICOH: 15'), icon: const Icon(Icons.print_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Scaners Keyence: 35'), icon: const Icon(Icons.scanner_rounded)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Scaners: 30'), icon: const Icon(Icons.scanner)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Raspberry Activas: 25'), icon: const Icon(Icons.usb)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Maquinas Planta: 35'), icon: const Icon(Icons.work_outline)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Antenas Activas: 80'), icon: const Icon(Icons.wifi)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.tv)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.add_box_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Terminales Activas: 45'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        FilledButton.icon(onPressed: () {}, label: const Text('Racks: 14'), icon: const Icon(Icons.laptop_chromebook_outlined)),
-        
-      ] ) ,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.push('/add');
-        },
-        foregroundColor: const Color(0xfffff5de),
-        backgroundColor: const Color(0xff35bf86),
-        child: const Icon(Icons.add),
-      ),
+      body: const _Preview() ,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: const _Menu(),
       drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
@@ -138,6 +82,96 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+    );
+  }
+}
+
+class _Preview extends StatelessWidget {
+  const _Preview();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children:[
+     CustomInfo(label: const Text('Equipo de Computo'), subtitle: const Text('Activos: 65'), icon: Image.asset('assets/images/1.gif'), button: const Text('More Info'), funcion: (){showDialog(context: context, builder: (context) => _Alert());}, color: const Color(0xfff1fbf5),),
+     CustomInfo(label: const Text('Impresoras'), subtitle: const Text('Activas: 50'), icon: Image.asset('assets/images/2.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Scanners'), subtitle: const Text('Activos: 50'), icon: Image.asset('assets/images/3.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Raspberry Pi'), subtitle: const Text('Activas: 25'), icon: Image.asset('assets/images/4.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Antenas'), subtitle: const Text('Activas: 18'), icon: Image.asset('assets/images/5.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Maquinas'), subtitle: const Text('Activas: 38'), icon: Image.asset('assets/images/6.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Terminales'), subtitle: const Text('Activas: 50'), icon: Image.asset('assets/images/7.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5)),
+     CustomInfo(label: const Text('Racks'), subtitle: const Text('Activas: 8'), icon: Image.asset('assets/images/8.gif'), button: const Text('More Info'), funcion: () {}, color: const Color(0xfff1fbf5))
+      
+    ]);
+  }
+}
+
+class _Alert extends StatelessWidget {
+  const _Alert();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      actions: [
+        TextButton(onPressed: () { Navigator.of(context).pop(); }, child: const Text('Close')),
+      ],
+      
+    );
+  }
+}
+
+class _Menu extends StatelessWidget {
+  const _Menu();
+
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      activeIcon: Icons.close_fullscreen,
+      iconTheme: const IconThemeData(color: Colors.white),
+      buttonSize: const Size(58, 58),
+      curve: Curves.bounceIn,
+      children: [
+        SpeedDialChild(
+          elevation: 0,
+          child: const Icon(
+          Icons.computer,
+          color: Colors.blue,
+        ),
+        labelWidget: const Text('Add computer', style: TextStyle(color: Colors.black54),),
+        backgroundColor: Colors.white30,
+        onTap: () {
+          context.push('/add');
+        },
+        ),
+        SpeedDialChild(
+          elevation: 0,
+          child: const Icon(
+          Icons.print,
+          color: Colors.blue,
+        ),
+        labelWidget: const Text('Add Printer', style: TextStyle(color: Colors.black54),),
+        backgroundColor: Colors.white30,
+        onTap: () {
+          context.push('/add');
+        },
+        ),
+        SpeedDialChild(
+          elevation: 0,
+          child: const Icon(
+          Icons.scanner,
+          color: Colors.blue,
+        ),
+        labelWidget: const Text('Add Scanner', style: TextStyle(color: Colors.black54),),
+        backgroundColor: Colors.white30,
+        onTap: () {
+          context.push('/add');
+        },
+        )
+      ],
+      child: const Icon(
+        Icons.menu_open_rounded,
+        color: Color(0xff1648c2),
+      ),
     );
   }
 }
--- lib/widgets/custom_info_tarjet.dart
+++ lib/widgets/custom_info_tarjet.dart
@@ -1,23 +1,48 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class CustomInfo extends StatelessWidget {
-  final Function() function;
+  //final Function() function;
   final Widget label;
-  final Icon icon;
-  final ButtonStyle style;
+  final Widget subtitle;
+  final Image icon;
+  final Color color;
+  //final ButtonStyle style;
+  final Widget button;
+  final Function() funcion;
 
   const CustomInfo ({super.key, 
-    required this.function,
+    //required this.function,
     required this.label,
+    required this.subtitle,
     required this.icon,
-    required this.style
+    //required this.style,
+    required this.button,
+    required this.funcion, 
+    required this.color,
 
   });
 
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(onPressed: function,  label: label, icon: icon, style: style);
+    return Card(
+      color: color,
+      shadowColor: color,
+      elevation: 0.5,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: icon,
+            title: label,
+            subtitle: subtitle,
+            trailing: TextButton(onPressed: funcion, child: button,),
+          ),
+        ],
+      ),
+    );
   }
 
 }
