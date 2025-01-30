1c140bc6caaf2870da1b1051e9a98c96609966e5
AndresOzil10 <andresref29@gmail.com>
2025-01-29 18:02:37 -0600

    start asing equipment

--- lib/screens/asignar_equipo_screen.dart
+++ lib/screens/asignar_equipo_screen.dart
@@ -1,81 +1,38 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:signature/signature.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
-
+import 'package:transfer_list/transfer_list.dart';
 
 class AsignarEquipoScreen extends StatelessWidget {
-  AsignarEquipoScreen({super.key});
-
-  final SignatureController _controller = SignatureController(
-    penStrokeWidth: 5,
-    penColor: Colors.black,
-    exportBackgroundColor: Colors.transparent,
-  );
-  void _generatePdf() async {
-    final pdf = pw.Document();
-    final signatureImage = await _controller.toImage();
-    final signatureBytes = await signatureImage?.toByteData(format: ImageByteFormat.png);
-    final signatureData = signatureBytes!.buffer.asUint8List();
-
-    pdf.addPage(
-      pw.Page(
-        build: (pw.Context context) => pw.Center(
-          child: pw.Column(
-            mainAxisAlignment: pw.MainAxisAlignment.center,
-            children: [
-              pw.Text('Documento con firma'),
-              pw.SizedBox(height: 20),
-              pw.Image(pw.MemoryImage(signatureData)),
-            ],
-          ),
-        ),
-      ),
-    );
-
-    await Printing.layoutPdf(onLayout: (PdfPageFormat format) async => pdf.save());
-  }
+  const AsignarEquipoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
+    return Scaffold(
+       appBar: AppBar(
           backgroundColor: const Color(0xff364461),
           foregroundColor: const Color(0xffe0e4ce),  //Color de letra
           centerTitle: true,
           title: const Text("Assign Equipment"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: _generatePdf,
-            ),
-          ],
         ),
-        body: Column(
-        children: [
-          Expanded(
-            child: Signature(
-              controller: _controller,
-              height: 300,
-              backgroundColor: Colors.grey[200]!,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  _controller.clear();
-                },
-                child: const Text('Limpiar'),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: TransferList(
+              leftList: const ['Dog', 'Cat', 'Mouse', 'Rabbit', 'Sheep'],
+              rightList: const ['Apple', 'Banana', 'Orange', 'Pineapple', 'Strawberry'],
+              onChange: (leftList, rightList) {
+                print('Left list: $leftList');
+                print('Right list: $rightList');
+              },
+              checkboxFillColor: const Color(0xff364461),
+              tileSplashColor: Colors.amber,
+              listBackgroundColor: Colors.grey.shade200,
+              textStyle: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          )
+        ),
     );
   }
 }
---
+++ lib/screens/assign_equipment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AssignEquipment extends StatelessWidget {
+  const AssignEquipment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
